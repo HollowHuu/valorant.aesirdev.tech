e22cb4c35682ab04817161e3fc12ad742a6e0ef4
8BitBlimp <63862081+8BitBlimp@users.noreply.github.com>
2023-04-29 11:51:48 +0200

    riot.txt

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,15 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
-      <head>
-        <title>Lunar</title>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
-      <h1>Lunar</h1>
-      <p>Welcome to Lunar. A closed network Valorant bot and Dashboard made for the Eclipse team.</p>
-      <br/>
-      <p>Currently in development, made by <a href="https://github.com/HollowHuu">HollowHuu</a></p>
+    <>
+      <div>
+      
+        <h1>Lunar</h1>
+        <p>Welcome to Lunar. A closed network Valorant bot and Dashboard made for the Eclipse team.</p>
+        <br/>
+        <p>Currently in development, made by <a href="https://github.com/HollowHuu">HollowHuu</a></p>
     </div>
+    </>
+
   )
 }
