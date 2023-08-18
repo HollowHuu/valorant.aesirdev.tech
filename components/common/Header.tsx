f133ca04f706ab769344e0f7f4b0fd86724d3421
Nicolas Wulff-Kähler <63862081+HollowHuu@users.noreply.github.com>
2023-08-18 07:57:56 +0200

    please work

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link"
+import { useTheme } from "next-themes";
 
 export default function DashboardHeader() {
+    const { theme, setTheme } = useTheme();
     return (
 
         <div
@@ -15,6 +17,16 @@ export default function DashboardHeader() {
                     <Link href="search" className='p-element'>Search</Link>
                     <Link href="profile" className='p-element'>Profile</Link>
                     <Link href="settings" className='p-element'>Settings</Link>
+                </div>
+                {/* Dark mode button at the very right */}
+                <div className="flex items-center justify-center gap-2 md:gap-8">
+                    <button
+                        aria-label="Toggle Dark Mode"
+                        type="button"
+                        className="w-10 h-10 p-3 rounded focus:outline-none"
+                        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+                        {theme === "dark" ? "🌙" : "☀️"}
+                    </button>
                 </div>
             </div>
         </div>
